e33013c695e12de2f74cc8f209ddd992b8d4aec2
Derek Thompson <dthompson@rationalenterprise.com>
2023-05-26 22:17:09 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function sayHello() {
+    alert('hello')
+}
+
+
+function sayGoodbye() {
+    alert('goodbye')
+}
+
+
+function showHello() {
+    document.getElementById('texthere').innerHTML = "hello"
+}
+
+function showGoodbye() {
+    document.getElementById('texthere').innerHTML = "goodbye"
+}
